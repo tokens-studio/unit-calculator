689b5c13badf67dcfbf5d7c8a8cc53e027bef23c
Florian Schroedl <flo.schroedl@gmail.com>
2025-06-03 14:39:31 +0200

    refactor: simplify config creation with default units and destructuring

--- src/config.ts
+++ src/config.ts
@@ -8,16 +8,12 @@ export interface CalcOptions {
   allowedUnits?: Set<string> | string[];
 }
 
-export function createConfig(options: CalcOptions = {}): CalcConfig {
-  let allowedUnits: Set<string>;
-  
-  if (options.allowedUnits instanceof Set) {
-    allowedUnits = options.allowedUnits;
-  } else if (options.allowedUnits) {
-    allowedUnits = new Set(options.allowedUnits);
-  } else {
-    allowedUnits = new Set(CSS_UNITS);
-  }
-  
-  return { allowedUnits };
+export const defaultConfig = {
+  allowedUnits: new Set(CSS_UNITS),
+};
+
+export function createConfig({
+  allowedUnits = new Set(CSS_UNITS),
+}: CalcConfig): CalcConfig {
+  return { allowedUnits: new Set(allowedUnits) };
 }

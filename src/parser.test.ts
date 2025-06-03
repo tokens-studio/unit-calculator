368c15c494a60d5f2ce8facc3f4c79a0742d3634
Florian Schroedl <flo.schroedl@gmail.com>
2025-06-03 12:51:54 +0200

    refactor: Move multi value tests

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -107,3 +107,24 @@ describe("CSS unit calculations", () => {
     expect(() => calc("1px / 1rem")).toThrow();
   });
 });
+
+describe("Multi-value expressions", () => {
+  it("Multiple expressions return array of values", () => {
+    expect(calc("1+1 2em * 3")).toEqual([2, "6em"]);
+    expect(calc("1+1 2em * 3 + 1em 2px")).toEqual([2, "7em", "2px"]);
+  });
+
+  it("calcMulti always returns array", () => {
+    expect(calc("1+1")).toEqual(2);
+    expect(calc("1+1 2em * 3")).toEqual([2, "6em"]);
+  });
+
+  it("Handles expressions with parentheses", () => {
+    expect(calc("(1+2) (3*4)")).toEqual([3, 12]);
+    expect(calc("(2em + 1em) (4px * 2)")).toEqual(["3em", "8px"]);
+  });
+
+  it("Ignores extra whitespace", () => {
+    expect(calc("  1+1   2em * 3  ")).toEqual([2, "6em"]);
+  });
+});

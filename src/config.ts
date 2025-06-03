f96859bab688e22982050cc3b5c9c3caa4124040
Florian Schroedl <flo.schroedl@gmail.com>
2025-06-03 14:44:44 +0200

    Defaults

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,8 @@ export const defaultConfig: CalcConfig = {
   allowedUnits: new Set(CSS_UNITS),
 };
 
-export function createConfig(options: Partial<CalcConfig> = {}): CalcConfig {
-  return { allowedUnits: new Set(options.allowedUnits || CSS_UNITS) };
+export function createConfig({
+  allowedUnits = defaultConfig.allowedUnits,
+}: Partial<CalcConfig> = {}): CalcConfig {
+  return { allowedUnits: new Set(allowedUnits) };
 }
